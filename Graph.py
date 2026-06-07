54d5a8052559d6ecda6ad0bd7ecb62e1e4ebe2e8
jackapolinarisa-25 <jackapolinarisa@gmail.com>
2026-06-07 16:59:37 +0800

    Add graph representation of locations and distances
    This file defines a graph representing locations and distances between them.

---
+++ Graph.py
@@ -0,0 +1,33 @@
+graph = {
+    "Labuan Bajo": {
+        "Pulau Kanawa": 10,
+        "Gua Rangko": 15,
+        "Pulau Padar": 35
+    },
+
+    "Pulau Kanawa": {
+        "Labuan Bajo": 10,
+        "Gua Rangko": 18,
+        "Pink Beach": 25
+    },
+
+    "Gua Rangko": {
+        "Labuan Bajo": 15,
+        "Pulau Kanawa": 18
+    },
+
+    "Pulau Padar": {
+        "Labuan Bajo": 35,
+        "Pulau Komodo": 12
+    },
+
+    "Pulau Komodo": {
+        "Pulau Padar": 12,
+        "Pink Beach": 8
+    },
+
+    "Pink Beach": {
+        "Pulau Komodo": 8,
+        "Pulau Kanawa": 25
+    }
+}
